9c40b78928b2eb6477dae4703a74369573ca37e7
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-25 17:20:02 -0600

    tested API key

--- src/views/TestingPage.vue
+++ src/views/TestingPage.vue
@@ -5,8 +5,43 @@
 </template>
 
 <script>
+import axios from 'axios';
+
     export default {
         
+
+
+        mounted(){
+
+
+            axios({
+
+                method: `GET`,
+
+                url:`https://foodie.bymoen.codes/api/validate`,
+
+
+                headers:{
+
+                    'x-api-key': `qK2iR1gTkkAjPH0kfGDY`
+
+
+                }
+
+            }).then((response) =>{
+
+
+                console.log(response);
+
+            }).catch((error) =>{
+
+                console.log(error);
+
+
+            })
+
+
+        }
     }
 </script>
 
