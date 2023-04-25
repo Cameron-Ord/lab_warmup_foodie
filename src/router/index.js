c10f5188ccb1cab7d868c65dbbafcd408c20b5c3
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-25 16:31:26 -0600

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,92 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import TestingPage from '@/views/TestingPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+
+    path: `/`,
+
+    component: TestingPage,
+
+    name: `testing page`,
+
+    meta: [{
+
+      title: `testing-page`
+
+    },
+
+    {
+
+      name: `description`,
+
+      content: `welcome to the testing page`
+
+    },
+    {
+
+      name: `author`,
+
+      content: `Cameron Ord`
+
+    },
+  
+  ]
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
+
+
 ]
 
 const router = new VueRouter({
   routes
 })
+
+router.beforeEach((to, from, next) => {
+
+  let current_meta_tags = document.querySelectorAll(`meta`);
+
+  for(let i = 0; i<current_meta_tags.length; i++){
+
+    current_meta_tags[i].remove();
+  }
+
+  let new_meta_tags = to[`meta`];
+
+  document.querySelector(`title`)[`innerText`] = new_meta_tags[0][`title`];
+
+  for(let i = 0; i<new_meta_tags.length; i++){
+
+    document.querySelector(`head`).insertAdjacentHTML(`beforeend`,
+    
+    `<meta name="${new_meta_tags[i][`name`]}"
+    
+    content="${new_meta_tags[i][`content`]}">`);
+
+
+  }
+
+  document.querySelector(`head`).insertAdjacentHTML(`afterbegin`,
+    
+  `<meta charset="utf-8">
+  
+  <meta http-equiv=""X-UA-Compatible" content="IE=edge">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">`
+  
+  );
+
+  from;
+
+  next();
+
+}
+);
+
+
 
 export default router
